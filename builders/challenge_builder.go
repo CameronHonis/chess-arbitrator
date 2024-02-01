222b97875427f82eff4e78e5d0a7a0a94443a1c5
Cameron <cameron-04@hotmail.com>
2024-02-01 09:43:47 -0600

    create a Challenge builder

--- builders/challenge_builder.go
+++ builders/challenge_builder.go
@@ -1,1 +1,81 @@
 package builders
+
+import (
+	"github.com/CameronHonis/chess-arbitrator/models"
+	"github.com/google/uuid"
+)
+
+func NewChallenge(challengerKey models.Key, challengedKey models.Key, isChallengerWhite bool,
+	isChallengerBlack bool, timeControl *models.TimeControl, botName string) *models.Challenge {
+
+	challengeId := uuid.New().String()
+
+	return &models.Challenge{
+		Uuid:              challengeId,
+		ChallengerKey:     challengerKey,
+		ChallengedKey:     challengedKey,
+		IsChallengerWhite: isChallengerWhite,
+		IsChallengerBlack: isChallengerBlack,
+		TimeControl:       timeControl,
+		BotName:           botName,
+	}
+}
+
+type ChallengeBuilder struct {
+	challenge *models.Challenge
+}
+
+func NewChallengeBuilder() *ChallengeBuilder {
+	return &ChallengeBuilder{
+		challenge: &models.Challenge{},
+	}
+}
+
+func (b *ChallengeBuilder) WithUuid(uuid string) *ChallengeBuilder {
+	b.challenge.Uuid = uuid
+	return b
+}
+
+func (b *ChallengeBuilder) WithRandomUuid() *ChallengeBuilder {
+	b.challenge.Uuid = uuid.New().String()
+	return b
+}
+
+func (b *ChallengeBuilder) WithChallengerKey(key models.Key) *ChallengeBuilder {
+	b.challenge.ChallengerKey = key
+	return b
+}
+
+func (b *ChallengeBuilder) WithChallengedKey(key models.Key) *ChallengeBuilder {
+	b.challenge.ChallengedKey = key
+	return b
+}
+
+func (b *ChallengeBuilder) WithIsChallengerWhite(isWhite bool) *ChallengeBuilder {
+	b.challenge.IsChallengerWhite = isWhite
+	return b
+}
+
+func (b *ChallengeBuilder) WithIsChallengerBlack(isBlack bool) *ChallengeBuilder {
+	b.challenge.IsChallengerBlack = isBlack
+	return b
+}
+
+func (b *ChallengeBuilder) WithTimeControl(timeControl *models.TimeControl) *ChallengeBuilder {
+	b.challenge.TimeControl = timeControl
+	return b
+}
+
+func (b *ChallengeBuilder) WithBotName(botName string) *ChallengeBuilder {
+	b.challenge.BotName = botName
+	return b
+}
+
+func (b *ChallengeBuilder) FromChallenge(challenge *models.Challenge) *ChallengeBuilder {
+	b.challenge = challenge
+	return b
+}
+
+func (b *ChallengeBuilder) Build() *models.Challenge {
+	return b.challenge
+}
--- models/challenge.go
+++ models/challenge.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"fmt"
-	"github.com/google/uuid"
 )
 
 type Challenge struct {
@@ -13,23 +12,6 @@ type Challenge struct {
 	IsChallengerBlack bool         `json:"isChallengerBlack"`
 	TimeControl       *TimeControl `json:"timeControl"`
 	BotName           string       `json:"botName"`
-}
-
-func NewChallenge(challengerKey Key, challengedKey Key, isChallengerWhite bool,
-	isColorsRandom bool, timeControl *TimeControl, botName string) *Challenge {
-	challengeId := uuid.New().String()
-	if isColorsRandom {
-
-	}
-	return &Challenge{
-		Uuid:              challengeId,
-		ChallengerKey:     challengerKey,
-		ChallengedKey:     challengedKey,
-		IsChallengerWhite: true,
-		IsChallengerBlack: false,
-		TimeControl:       timeControl,
-		BotName:           botName,
-	}
 }
 
 func (c *Challenge) Topic() MessageTopic {
