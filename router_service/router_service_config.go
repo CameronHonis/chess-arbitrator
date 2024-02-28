fe99ad08f6e4e3bcb3816bd5c1edd1126d419ea1
Cameron <cameron-04@hotmail.com>
2024-02-27 21:12:31 -0600

    add envvar lookups when setting server port

--- router_service/router_service_config.go
+++ router_service/router_service_config.go
@@ -1,14 +1,25 @@
 package router_service
 
-import "github.com/CameronHonis/service"
+import (
+	"github.com/CameronHonis/service"
+	"os"
+	"strconv"
+)
 
 type RouterServiceConfig struct {
 	Port uint
 }
 
 func NewRouterServiceConfig() *RouterServiceConfig {
+	portEnvVal, _ := os.LookupEnv("LISTEN_PORT")
+	var port uint
+	if num, err := strconv.Atoi(portEnvVal); err == nil {
+		port = uint(num)
+	} else {
+		port = 8080
+	}
 	return &RouterServiceConfig{
-		Port: 8080,
+		Port: port,
 	}
 }
 
